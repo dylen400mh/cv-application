b11cfe646287d3621a1fb4449decf43a88688029
Dylen <dylenbelanger2004@gmail.com>
2023-09-01 19:20:04 -0400

    Add alignment styling for GeneralInfoForm component.

--- src/components/GeneralInfoForm.jsx
+++ src/components/GeneralInfoForm.jsx
@@ -1,4 +1,6 @@
 /* eslint-disable react/prop-types */
+import "../styles/GeneralInfoForm.css";
+
 function GeneralInfoForm({ data, handleChange, handleSubmit }) {
   const handleNameChange = (e) => {
     handleChange({ ...data, name: e.target.value });
@@ -13,29 +15,35 @@ function GeneralInfoForm({ data, handleChange, handleSubmit }) {
   };
 
   return (
-    <div>
+    <div className="generalInfo">
       <h1>General Info</h1>
-      <label htmlFor="">Full Name</label>
-      <input
-        type="text"
-        placeholder="Full Name"
-        value={data.name}
-        onChange={handleNameChange}
-      />
-      <label htmlFor="">Email</label>
-      <input
-        type="text"
-        placeholder="Email"
-        value={data.email}
-        onChange={handleEmailChange}
-      />
-      <label htmlFor="">Phone Number</label>
-      <input
-        type="text"
-        placeholder="Phone Number"
-        value={data.phone}
-        onChange={handlePhoneChange}
-      />
+      <div className="container">
+        <label htmlFor="">Full Name</label>
+        <input
+          type="text"
+          placeholder="Full Name"
+          value={data.name}
+          onChange={handleNameChange}
+        />
+      </div>
+      <div className="container">
+        <label htmlFor="">Email</label>
+        <input
+          type="text"
+          placeholder="Email"
+          value={data.email}
+          onChange={handleEmailChange}
+        />
+      </div>
+      <div className="container">
+        <label htmlFor="">Phone Number</label>
+        <input
+          type="text"
+          placeholder="Phone Number"
+          value={data.phone}
+          onChange={handlePhoneChange}
+        />
+      </div>
       <button onClick={handleSubmit}>Submit</button>
     </div>
   );
